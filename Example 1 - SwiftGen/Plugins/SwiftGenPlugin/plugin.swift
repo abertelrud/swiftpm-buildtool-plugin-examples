7d1f2b75b5fa34b684231549cc4db26a3c7663f3
Anders Bertelrud <anders@apple.com>
2021-12-22 22:13:10 -0800

    Adjust sample packages to SE-0332 changes

--- Example 1 - SwiftGen/Plugins/SwiftGenPlugin/plugin.swift
+++ Example 1 - SwiftGen/Plugins/SwiftGenPlugin/plugin.swift
@@ -4,15 +4,15 @@ import Foundation
 @main
 struct SwiftGenPlugin: BuildToolPlugin {
     
-    func createBuildCommands(context: TargetBuildContext) throws -> [Command] {
+    func createBuildCommands(context: PluginContext, target: Target) throws -> [Command] {
         // This example configures `swiftgen` to take inputs from a `swiftgen.yml` file.
-        let swiftGenConfigFile = context.packageDirectory.appending("swiftgen.yml")
+        let swiftGenConfigFile = context.package.directory.appending("swiftgen.yml")
 
         // This example configures `swiftgen` to write to a "SwiftGenOutputs" directory.
         let swiftGenOutputsDir = context.pluginWorkDirectory.appending("SwiftGenOutputs")
         try FileManager.default.createDirectory(atPath: swiftGenOutputsDir.string, withIntermediateDirectories: true)
 
-        // Create a command to run `swiftgen` as a prebuild command. It will be run before
+        // Return a command to run `swiftgen` as a prebuild command. It will be run before
         // every build and generates source files into an output directory provided by the
         // build context.
         return [.prebuildCommand(
@@ -25,8 +25,8 @@ struct SwiftGenPlugin: BuildToolPlugin {
                 "--config", "\(swiftGenConfigFile)"
             ],
             environment: [
-                "PROJECT_DIR": "\(context.packageDirectory)",
-                "TARGET_NAME": "\(context.targetName)",
+                "PROJECT_DIR": "\(context.package.directory)",
+                "TARGET_NAME": "\(target.name)",
                 "DERIVED_SOURCES_DIR": "\(swiftGenOutputsDir)",
             ],
             outputFilesDirectory: swiftGenOutputsDir)
